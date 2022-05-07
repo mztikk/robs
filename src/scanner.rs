cc4cd9fbcc71db10c7f07766e9fdb540275271ab
Nils Feierabend <mztikk@outlook.de>
2022-05-07 11:36:51 +0200

    refactored aob scanning into more consice functional calls

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,6 @@
-use std::{ops::Index, usize};
-
 use crate::signature::Signature;
+use itertools::multizip;
+use std::usize;
 
 pub trait AobScanner {
     fn find_signature(&self, signature: &Signature) -> Option<usize>;
@@ -25,36 +25,102 @@ impl AobScanner for Vec<u8> {
 }
 
 pub fn find_signature(search_region: &[u8], signature: &Signature) -> Option<usize> {
-    let first_index = signature.first_byte.unwrap();
+    let first_index = signature.first_byte?;
     let first_item = signature.pattern[first_index];
+    let mask_len = signature.mask.len();
 
-    let mut i: usize = 0;
-    while i < search_region.len() - signature.pattern.len() {
-        let find = search_region[i..].iter().position(|&x| x == first_item)?;
-
-        i += find;
-
-        let (check, delta) = check_mask(&search_region[i..], &signature);
-        if check {
-            return Some(i + signature.offset);
-        }
-
-        i += delta;
-    }
-
-    None
+    search_region
+        .iter()
+        .position(|&item| item == first_item)
+        .and_then(|index| {
+            check_mask(&search_region[index..index + mask_len], signature)
+                .then(|| index + signature.offset)
+        })
 }
 
-fn check_mask<T: ?Sized>(search_region: &T, signature: &Signature) -> (bool, usize)
-where
-    T: Index<usize, Output = u8>,
-{
-    let len = signature.pattern.len();
-    for i in 0..len {
-        if signature.mask[i] != '?' && signature.pattern[i] != search_region[i] {
-            return (false, i);
-        }
+fn check_mask(search_region: &[u8], signature: &Signature) -> bool {
+    multizip((
+        search_region.iter(),
+        signature.mask.iter(),
+        signature.pattern.iter(),
+    ))
+    .all(|(item, mask, pattern)| match mask {
+        '?' => true,
+        'x' => item == pattern,
+        _ => false,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        scanner::{check_mask, find_signature},
+        signature::Signature,
+    };
+
+    #[test]
+    fn test_check_mask() {
+        let search_region = &[0x0B, 0x0C, 0x0D, 0x0E, 0x0F];
+        let signature = Signature::new("0B ?? 0D", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let check = check_mask(search_region, &signature);
+        assert!(check);
     }
 
-    (true, len)
+    #[test]
+    fn test_find_signature() {
+        let search_region = &[
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F,
+        ];
+        let signature = Signature::new("0B ?? 0D", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let find = find_signature(search_region, &signature);
+        assert!(find.is_some());
+        let find = find.unwrap();
+        assert_eq!(find, 11);
+    }
+
+    #[test]
+    fn test_signature_not_found() {
+        let search_region = &[
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F,
+        ];
+        let signature = Signature::new("FF FF", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let find = find_signature(search_region, &signature);
+        assert!(find.is_none());
+    }
+
+    #[test]
+    fn test_signature_exact() {
+        let search_region = &[
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F,
+        ];
+        let signature = Signature::new("00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let find = find_signature(search_region, &signature);
+        assert!(find.is_some());
+        let find = find.unwrap();
+        assert_eq!(find, 0);
+    }
+
+    #[test]
+    fn test_signature_exact_except_last() {
+        let search_region = &[
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F,
+        ];
+        let signature = Signature::new("00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E FF", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let find = find_signature(search_region, &signature);
+        assert!(find.is_none());
+    }
 }
