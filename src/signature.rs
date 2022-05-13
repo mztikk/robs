151dd2a773df688c16c5ae7d31daea4aad9abb38
mztikk <mztikk@outlook.de>
2022-05-13 22:14:30 +0200

    better error handling for signatures

--- src/signature.rs
+++ src/signature.rs
@@ -1,10 +1,12 @@
-use std::{error, fmt, usize};
-
+use std::{fmt, usize};
 use stringr::Stringr;
+use thiserror::Error;
 
+#[derive(Debug)]
 pub struct Signature {
     pub first_wildcard: Option<usize>,
     pub first_byte: Option<usize>,
+    pub matching_indices: Vec<usize>,
     pub length: usize,
     pub pattern: Vec<u8>,
     pub mask: Vec<char>,
@@ -14,33 +16,30 @@ pub struct Signature {
 
 impl fmt::Display for Signature {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", &Signature::format(&self.sig)?)
+        match Signature::format(&self.sig) {
+            Ok(s) => write!(f, "{}", s),
+            Err(e) => write!(f, "{}", e),
+        }
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct SignatureLengthError;
-impl error::Error for SignatureLengthError {}
-
-impl fmt::Display for SignatureLengthError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "signature length(excluding whitespace) must be divisible by 2, make sure to prepend bytes with 0 if necessary and make wildcards full ?? instead of single ?")
-    }
-}
-
-impl From<SignatureLengthError> for fmt::Error {
-    fn from(_: SignatureLengthError) -> Self {
-        fmt::Error
-    }
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
+pub enum SignatureParseError {
+    #[error(
+        "Signature length(excluding whitespace) must be divisible by 2, but was of length {0}"
+    )]
+    InvalidLength(usize),
+    #[error("Signature must contain only hexadecimal characters, but was {0}")]
+    InvalidString(String),
 }
 
 impl Signature {
     pub fn get_pattern_and_mask_from_signature(
         signature: &str,
-    ) -> Result<(Vec<u8>, Vec<char>), Box<dyn error::Error>> {
+    ) -> Result<(Vec<u8>, Vec<char>), SignatureParseError> {
         let signature = signature.remove_whitespace();
         if signature.len() % 2 != 0 {
-            Err(SignatureLengthError.into())
+            Err(SignatureParseError::InvalidLength(signature.len()))
         } else {
             let split = signature.splitn(2);
             let mut bytes = Vec::with_capacity(split.len());
@@ -54,7 +53,7 @@ impl Signature {
                         Ok(v) => {
                             bytes.push(v);
                         }
-                        Err(e) => return Err(e.into()),
+                        Err(_e) => return Err(SignatureParseError::InvalidString(c)),
                     }
                     mask.push('x');
                 }
@@ -63,12 +62,18 @@ impl Signature {
         }
     }
 
-    pub fn new(signature: &str, offset: usize) -> Result<Signature, Box<dyn error::Error>> {
+    pub fn new(signature: &str, offset: usize) -> Result<Signature, SignatureParseError> {
         let (pattern, mask) = Signature::get_pattern_and_mask_from_signature(signature)?;
+        let indices = mask
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &m)| if m == 'x' { Some(i) } else { None })
+            .collect();
 
         return Ok(Signature {
             first_wildcard: mask.iter().position(|&c| c == '?'),
             first_byte: mask.iter().position(|&c| c == 'x'),
+            matching_indices: indices,
             length: pattern.len(),
             pattern,
             mask,
@@ -77,12 +82,37 @@ impl Signature {
         });
     }
 
-    pub fn format(signature: &str) -> Result<String, SignatureLengthError> {
+    pub fn format(signature: &str) -> Result<String, SignatureParseError> {
         let signature = signature.remove_whitespace();
         if signature.len() % 2 != 0 {
-            return Err(SignatureLengthError);
+            return Err(SignatureParseError::InvalidLength(signature.len()));
         }
 
         Ok(signature.splitn_separator(2, " ").to_ascii_uppercase())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_invalid_length() {
+        let signature = "123";
+        let signature = super::Signature::new(signature, 0);
+        assert!(signature.is_err());
+        assert_eq!(
+            signature.unwrap_err(),
+            super::SignatureParseError::InvalidLength(3)
+        );
+    }
+
+    #[test]
+    fn test_invalid_string() {
+        let signature = "zz";
+        let signature = super::Signature::new(signature, 0);
+        assert!(signature.is_err());
+        assert_eq!(
+            signature.unwrap_err(),
+            super::SignatureParseError::InvalidString("zz".to_string())
+        );
     }
 }
