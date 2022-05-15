bcf2144eec1a2aad6802ab3bfde7a7eebb0dd58e
mztikk <mztikk@outlook.de>
2022-05-15 11:43:01 +0200

    fixed signature scanning which start with wildcards

--- src/scanner.rs
+++ src/scanner.rs
@@ -33,7 +33,12 @@ pub fn find_signature(search_region: &[u8], signature: &Signature) -> Option<usi
     search_region
         .iter()
         .positions(|&item| item == first_item)
-        .find(|&index| check_mask(&search_region[index..index + mask_len], signature))
+        .find(|&index| {
+            check_mask(
+                &search_region[(index - first_index)..(index + mask_len)],
+                signature,
+            )
+        })
         .map(|index| index + signature.offset)
 }
 
@@ -115,5 +120,20 @@ mod tests {
         let signature = signature.unwrap();
         let find = find_signature(search_region, &signature);
         assert!(find.is_none());
+    }
+
+    #[test]
+    fn test_signature_start_with_wildcard() {
+        let search_region = &[
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F, 0xFF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01,
+        ];
+        let signature = Signature::new("?? 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F", 0);
+        assert!(signature.is_ok());
+        let signature = signature.unwrap();
+        let find = find_signature(search_region, &signature);
+        assert!(find.is_some());
+        let find = find.unwrap();
+        assert_eq!(find, 1);
     }
 }
